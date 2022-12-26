d64652a76d239abca3266306746cbf2995ca2d79
fahiz677 <fahizp3@gmail.com>
2022-12-25 21:47:05 -0800

    6

--- helpers/otpHelpers.js
+++ helpers/otpHelpers.js
@@ -1,5 +1,5 @@
 module.exports={
     serviceId:"VA703053f65ee4f826ce253ba70ccb02f7",
     accoundSid:"AC8f1cedc7070864fcce18758108e0882e",
-    authToken:"07cc9aafed8d7796a9bbe8e3ab12416c"
+    authToken:"a3206738fc896a968c240994b5e6b490"
 }
